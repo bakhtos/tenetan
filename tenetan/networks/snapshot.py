4e6a453c37342881e9ded0590483d2a6c1146ad8
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2023-10-06 15:26:00 +0300

    add: initial project structure

---
+++ tenetan/networks/snapshot.py
@@ -0,0 +1,13 @@
+import tensorly as tl
+import numpy as np
+
+__all__ = ['SnapshotGraph']
+
+
+class SnapshotGraph:
+
+    def __init__(self, path, directed=True):
+
+        # TODO Load network from path
+        # If directed is true, n[i,j,t] =w; if false, also n[j,i,t] = w
+        pass
