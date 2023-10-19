cacb6d73b69d3839fdc437ee13382713b9455764
anika-bushra-chowdhury <anikabushra6069@gmail.com>
2023-10-19 16:56:28 +0600

    tensor created from numpy array

--- tenetan/networks/snapshot.py
+++ tenetan/networks/snapshot.py
@@ -1,5 +1,6 @@
 import tensorly as tl
 import numpy as np
+from tensorly import random
 
 __all__ = ['SnapshotGraph']
 
@@ -7,7 +8,14 @@ __all__ = ['SnapshotGraph']
 class SnapshotGraph:
 
     def __init__(self, path, directed=True):
+        self.path = path
+        self.directed = directed
 
         # TODO Load network from path
         # If directed is true, n[i,j,t] =w; if false, also n[j,i,t] = w
         pass
+
+    arr = np.loadtxt("D:/tenetan/tenetan/datasets/small.csv", delimiter=",")
+    tensor = tl.tensor(arr)
+    print(tensor)
+
