40a0f06c94340480e3a04e1a3ff04fde61ab698c
Ameen <mpameen789@gmail.com>
2023-10-20 18:20:33 +0300

    load data

--- tenetan/__init__.py
+++ tenetan/__init__.py
@@ -1,2 +1,7 @@
-from . import networks
-from . import community
+#from . import networks
+#from . import community
+
+import pandas as pd
+
+data = pd.read_csv(r"C:\Users\91974\Desktop\Software Project\tenetan\tenetan\datasets\small.csv")
+print(data)
