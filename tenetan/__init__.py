10e50ebb0dd6ec47a6fa73a9f1db37b7d97749e7
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2023-10-23 15:27:57 +0300

    Revert "load data"
    This reverts commit 40a0f06c94340480e3a04e1a3ff04fde61ab698c.

--- tenetan/__init__.py
+++ tenetan/__init__.py
@@ -1,7 +1,2 @@
-#from . import networks
-#from . import community
-
-import pandas as pd
-
-data = pd.read_csv(r"C:\Users\91974\Desktop\Software Project\tenetan\tenetan\datasets\small.csv")
-print(data)
+from . import networks
+from . import community
