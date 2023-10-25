f150bf86b00bbffc5e7ae7ab868ce7deb184ad8b
Ameen <mpameen789@gmail.com>
2023-10-25 15:41:48 +0300

    Data loaded into 3D Array changed. change - Float64 into integer

--- tenetan/networks/snapshot.py
+++ tenetan/networks/snapshot.py
@@ -10,10 +10,15 @@ class SnapshotGraph:
 
         # TODO Load network from path
         # If directed is true, n[i,j,t] =w; if false, also n[j,i,t] = w
-        data = np.loadtxt(path, delimiter=',')
+        data = np.loadtxt(path, delimiter=',', skiprows=1)
+        #coverting float64 into integer
+        data = data.astype(int)
         max_coords = np.max(data[:, :3], axis=0) + 1
         value_array = np.full((max_coords[0], max_coords[1], max_coords[2]), np.nan)
         for row in data:
             x, y, z, value = map(int, row)
             value_array[x, y, z] = value
+
+
+
 
