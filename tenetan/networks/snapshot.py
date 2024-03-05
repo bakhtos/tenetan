39deec1649bf6b5e9ff681326a7bdc701081615d
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-03-05 17:06:22 +0200

    fix: correctly parse tensor shape

--- tenetan/networks/snapshot.py
+++ tenetan/networks/snapshot.py
@@ -11,8 +11,10 @@ class SnapshotGraph:
         # TODO Load network from path
         # If directed is true, n[i,j,t] =w; if false, also n[j,i,t] = w
         data = np.loadtxt(path, dtype=np.float32, delimiter=',', comments='#')
-        max_coords = np.max(data[:, :3], axis=0) + 1
-        tensor = np.full((max_coords[0], max_coords[1], max_coords[2]), 0.0)
+        max_coords = np.max(data[:, :3], axis=0).astype(int) + 1
+        max_vertex = max(max_coords[0], max_coords[1])
+        max_time = max_coords[2]
+        tensor = np.full((max_vertex, max_vertex, max_time), 0.0)
         for row in data:
             i, j, t, w = int(row[0]), int(row[1]), int(row[2]), float(row[3])
             tensor[i, j, t] = w
