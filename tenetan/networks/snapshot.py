8b5a1ffbff4a0c5ecbdc8fe5bb5e29545da354ad
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-03-05 16:49:18 +0200

    fix: default value for tensor is 0.0

--- tenetan/networks/snapshot.py
+++ tenetan/networks/snapshot.py
@@ -11,13 +11,9 @@ class SnapshotGraph:
         # TODO Load network from path
         # If directed is true, n[i,j,t] =w; if false, also n[j,i,t] = w
         data = np.loadtxt(path, dtype=np.float32, delimiter=',', comments='#')
-        #coverting float64 into integer
         max_coords = np.max(data[:, :3], axis=0) + 1
-        value_array = np.full((max_coords[0], max_coords[1], max_coords[2]), np.nan)
+        value_array = np.full((max_coords[0], max_coords[1], max_coords[2]), 0.0)
         for row in data:
             x, y, z, value = map(int, row)
             value_array[x, y, z] = value
-
-
-
 
