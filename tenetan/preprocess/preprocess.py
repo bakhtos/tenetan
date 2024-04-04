1412a80867efc29b4c1340a81add285878dc48d8
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-04-04 18:39:27 +0300

    add: save new files alongside input file

--- tenetan/preprocess/preprocess.py
+++ tenetan/preprocess/preprocess.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+
 import pandas as pd
 
 __all__ = ['preprocess_directory', 'preprocess_names']
@@ -38,18 +40,19 @@ def preprocess_names(input_file, /, *, source_col='i', target_col='j', time_col=
     data['t'] = data[time_col].map(timestamp_index_mapping)
     data['w'] = data[weight_col]
 
+    input_file = str(pathlib.Path(input_file).with_suffix(''))
     if output_file is not None:
-        output_file = 'network.csv' if output_file == 'default' else output_file
+        output_file = f'{input_file}_network.csv' if output_file == 'default' else output_file
         data.to_csv(output_file, header=False, index=False, columns=['i', 'j', 't', 'w'])
 
     if vertex_file is not None:
-        vertex_file = 'vertices.txt' if vertex_file == 'default' else vertex_file
+        vertex_file = f'{input_file}_vertices.txt' if vertex_file == 'default' else vertex_file
         with open(vertex_file, 'w') as f:
             for vertex in vertex_list:
                 f.write(f'{vertex}\n')
 
     if timestamp_file is not None:
-        timestamp_file = 'timestamps.txt' if timestamp_file == 'default' else timestamp_file
+        timestamp_file = f'{input_file}_timestamps.txt' if timestamp_file == 'default' else timestamp_file
         with open(timestamp_file, 'w') as f:
             for timestamp in timestamp_list:
                 f.write(f'{timestamp}\n')
