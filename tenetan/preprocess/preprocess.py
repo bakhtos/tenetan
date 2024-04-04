3102dff53be084fabf627795c9488c09c73d81f5
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-04-04 20:43:17 +0300

    add: do not convert timestamps in process_dir, save with header

--- tenetan/preprocess/preprocess.py
+++ tenetan/preprocess/preprocess.py
@@ -9,9 +9,9 @@ __all__ = ['preprocess_directory', 'preprocess_names']
 def preprocess_directory(input_dir, /, *, output_file=None, source_col='i', target_col='j', weight_col='w'):
     files = sorted(os.listdir(input_dir))
     all_data = []
-    for time, file in enumerate(files):
+    for file in files:
         data = pd.read_csv(os.path.join(input_dir, file))
-        data['t'] = time
+        data['t'] = pathlib.Path(file).with_suffix('')
         all_data.append(data)
     all_data = pd.concat(all_data, ignore_index=True)
     all_data = all_data.rename(columns={source_col: 'i',
@@ -21,7 +21,7 @@ def preprocess_directory(input_dir, /, *, output_file=None, source_col='i', targ
     if output_file is not None:
         if output_file == 'default':
             output_file = f'{input_dir}_concat.csv'
-        all_data.to_csv(output_file, index=False, columns=['i', 'j', 't', 'w'], header=False)
+        all_data.to_csv(output_file, index=False, columns=['i', 'j', 't', 'w'], header=True)
     return all_data
 
 
