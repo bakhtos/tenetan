f794ceface3b7d5966c8178f5036ef4022662bc7
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-04-09 15:41:11 +0300

    ref: rename arguments

--- tenetan/community/parafac.py
+++ tenetan/community/parafac.py
@@ -5,17 +5,17 @@ import numpy as np
 __all__ = ['parafac_community']
 
 
-def parafac_community(N: SnapshotGraph, R: int, **tensorly_kwargs):
+def parafac_community(network: SnapshotGraph, n_communities: int, **tensorly_kwargs):
     """
     Gauvin L, Panisson A, Cattuto C (2014) Detecting the Community Structure and Activity Patterns of Temporal Networks:
     A Non-Negative Tensor Factorization Approach. PLoS ONE 9(1): e86028.
     https://doi.org/10.1371/journal.pone.0086028
-    :param N: Temporal network
-    :param R: Number of communities
+    :param network: Temporal network
+    :param n_communities: Number of communities to detect
     :param tensorly_kwargs: kwargs to pass to tensorly.decomposition.non_negative_parafac_hals
     :return:
     """
 
-    cp_tensor, errors = tl.decomposition.non_negative_parafac_hals(N._tensor, R, return_errors=True, **tensorly_kwargs)
-    in_communities, out_communities, time_activity = cp_tensor.factors
-    return in_communities, out_communities, time_activity, errors
+    cp_tensor, errors = tl.decomposition.non_negative_parafac_hals(network._tensor, n_communities, return_errors=True, **tensorly_kwargs)
+    in_communities, out_communities, raw_temporal_activity = cp_tensor.factors
+    return in_communities, out_communities, raw_temporal_activity, errors
