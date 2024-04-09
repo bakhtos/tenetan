2201d05cda109eb0350c0d4f1219e6ab44ddcb90
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2024-04-09 15:35:47 +0300

    add: pass network to parafac decomposition

--- tenetan/community/parafac.py
+++ tenetan/community/parafac.py
@@ -1,10 +1,11 @@
+from tenetan.networks import SnapshotGraph
 import tensorly as tl
 import numpy as np
 
 __all__ = ['parafac_community']
 
 
-def parafac_community(N, R):
+def parafac_community(N: SnapshotGraph, R: int):
     """
     Gauvin L, Panisson A, Cattuto C (2014) Detecting the Community Structure and Activity Patterns of Temporal Networks:
     A Non-Negative Tensor Factorization Approach. PLoS ONE 9(1): e86028.
@@ -14,5 +15,6 @@ def parafac_community(N, R):
     :return:
     """
 
-    # TODO Implement community detection from non-negative parafac
-    pass
+    cp_tensor, errors = tl.decomposition.non_negative_parafac_hals(N._tensor, R, return_errors=True)
+    in_communities, out_communities, time_activity = cp_tensor.factors
+    return in_communities, out_communities, time_activity, errors
